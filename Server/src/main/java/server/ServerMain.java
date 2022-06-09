e675b47babad99e0d66258f031a043449fbeeb59
Mohammad <mhmddrszz@gmail.com>
2022-06-09 11:39:35 +0430

    myTestSeyyed

--- Server/src/main/java/server/ServerMain.java
+++ Server/src/main/java/server/ServerMain.java
@@ -3,5 +3,6 @@ package server;
 public class ServerMain {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Hello be khodet");
     }
 }
