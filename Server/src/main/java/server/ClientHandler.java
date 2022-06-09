6747cf5f15616009fc71e78d0f4e57927594b9e8
Mohammad <mhmddrszz@gmail.com>
2022-06-09 15:21:36 +0430

    Added some code to Server

--- Server/src/main/java/server/ClientHandler.java
+++ Server/src/main/java/server/ClientHandler.java
@@ -1,8 +1,43 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientHandler implements Runnable{
+    private static List<ClientHandler> clientHandlers = new ArrayList<>();
+    // Socket for a connection, buffer reader and writer for receiving and sending data respectively.
+    private Socket socket;
+    private InputStreamReader inputStreamReader;
+    private OutputStreamWriter outputStreamWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.inputStreamReader = new InputStreamReader(socket.getInputStream());
+            this.outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            //when a client connects their username is sent.
+            this.clientUsername = new BufferedReader(inputStreamReader).readLine();
+            clientHandlers.add(this);
+        }catch (IOException e){
+            e.printStackTrace();
+            //tODO : should write a function that close everything.
+        }
+    }
+
     @Override
     public void run() {
-
+        while (socket.isConnected()){
+            try {
+                System.out.println(new BufferedReader(inputStreamReader).readLine());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -1,4 +1,52 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerStatus status;
+    private ServerSocket serverSocket;
+
+    //
+    Server(){
+        status = ServerStatus.WAITING;
+    }
+
+    /*initializing the Server, and putting the socket port on 8000
+     waiting for clients to join.
+    */
+    public void initialize(){
+        System.out.println("Server is " + status);
+        try{
+            ServerSocket serverSocket = new ServerSocket(8000);
+            while (!serverSocket.isClosed()){
+                //Server socket will be closed in Client Handler.
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                // The start method begins the execution of a thread.
+                thread.start();;
+            }
+        }catch (IOException e){
+            //tODO : either the stack trace or close server is enough we can delete it later on.
+            e.printStackTrace();
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+// if Server is listening for new Clients : Waiting
+// if Server is playing the Game : Running
+enum ServerStatus {
+    WAITING , RUNNING;
 }
