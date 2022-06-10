fb11eb2985f5749f2ff50c812546bc4ede1335bf
Mohammad <mhmddrszz@gmail.com>
2022-06-10 18:18:46 +0430

    reCreated ClientHandler.java

--- Server/src/main/java/server/ClientHandler.java
+++ Server/src/main/java/server/ClientHandler.java
@@ -1,30 +1,25 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import logic.game.Game;
+import logic.player.MyPlayer;
+
+import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class ClientHandler implements Runnable{
     // Socket for a connection, buffer reader and writer for receiving and sending data respectively.
     private Socket socket;
-    private InputStreamReader inputStreamReader;
-    private OutputStreamWriter outputStreamWriter;
-    private String clientUsername;
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
 
     public ClientHandler(Socket socket) {
+        System.out.println("new client handler created.");
         try {
             this.socket = socket;
-            this.inputStreamReader = new InputStreamReader(socket.getInputStream());
-            this.outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.printWriter = new PrintWriter(socket.getOutputStream());
             //when a client connects their username is sent.
-            outputStreamWriter.write("Enter username : ");
-            this.clientUsername = new BufferedReader(inputStreamReader).readLine();
-
-
         }catch (IOException e){
             e.printStackTrace();
             //tODO : should write a function that close everything.
@@ -33,12 +28,36 @@ public class ClientHandler implements Runnable{
 
     @Override
     public void run() {
-        while (socket.isConnected()){
-            try {
-                System.out.println(new BufferedReader(inputStreamReader).readLine());
-            }catch (IOException e){
-                e.printStackTrace();
-            }
+        if (wantsToPlayGame()){
+            MyPlayer myPlayer = new MyPlayer();
+            Game game = new Game(myPlayer , numberOfBots());
         }
+    }
+    private boolean wantsToPlayGame(){
+        try {
+            sendMessage("Do you want to play the mind-game?(y/n)");
+            String res = bufferedReader.readLine();
+            if (res.equals("y")) {
+                return true;
+            }else {
+                return wantsToPlayGame();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }return wantsToPlayGame();
+    }
+    private int numberOfBots(){
+        sendMessage("How many bots you want to add?");
+        try {
+            return Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numberOfBots();
+    }
+
+    public void sendMessage(String message){
+        printWriter.println(message);
+        printWriter.flush();
     }
 }
