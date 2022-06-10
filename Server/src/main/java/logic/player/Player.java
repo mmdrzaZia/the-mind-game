1c7ade951790c3151b70f4e07bde609feb75a9ea
Mohammad <mhmddrszz@gmail.com>
2022-06-10 18:33:27 +0430

    updated Player.java

--- Server/src/main/java/logic/player/Player.java
+++ Server/src/main/java/logic/player/Player.java
@@ -1,4 +1,14 @@
 package logic.player;
 
+import logic.game.Card;
+
+import java.util.List;
+
 public abstract class Player {
+    protected List<Card> hand;
+    protected boolean isAlive;
+
+    //playStrategy?????
+
+    //checkIfAlive()
 }
